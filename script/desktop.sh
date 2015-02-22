df6edd55ab90af06f6aa47e1f9ffcfaf41286f90
Victor Volle <victor.volle@beta-thoughts.org>
2015-02-22 23:38:27 +0100

    make centos70 templates as same as possible

--- script/desktop.sh
+++ script/desktop.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-USERNAME=vagrant
+if [[ "${INSTALL_DESKTOP:-yes}" == "yes" ]]; then
 
-cat /etc/redhat-release
-if grep -q -i "release 7" /etc/redhat-release ; then
-    # set the graphical.target for systemd so it will run startx on boot
-    ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
-fi
+  USERNAME=vagrant
 
-GDM_CONFIG=/etc/gdm/custom.conf
+  cat /etc/redhat-release
+  if grep -q -i "release 7" /etc/redhat-release ; then
+      # set the graphical.target for systemd so it will run startx on boot
+      ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+  fi
 
-# Configure gdm autologin.
+  GDM_CONFIG=/etc/gdm/custom.conf
 
-if [ -f $GDM_CONFIG ]; then
-    sed -i s/"daemon]$"/"daemon]\nAutomaticLoginEnable=true\nAutomaticLogin=vagrant"/ /etc/gdm/custom.conf
+  # Configure gdm autologin.
+
+  if [ -f $GDM_CONFIG ]; then
+      sed -i s/"daemon]$"/"daemon]\nAutomaticLoginEnable=true\nAutomaticLogin=vagrant"/ /etc/gdm/custom.conf
+  fi
+
 fi
